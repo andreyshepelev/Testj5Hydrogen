c68bea6a9efada0f2b1b78499139f674fd151267
Andrey Shepelev <andrei.s@pebblepost.com>
2024-06-20 14:27:30 +0300

    change js file to another filepath

--- app/root.tsx
+++ app/root.tsx
@@ -113,7 +113,7 @@ export default function App() {
         <Script
             id="pebblepost_pixel"
             type="text/javascript"
-            src="https://testj5shop.free.nf/2525.js"
+            src="https://testj5shop.free.nf/2525.js?i=1"
         />
         <Scripts nonce={nonce} />
       </body>
